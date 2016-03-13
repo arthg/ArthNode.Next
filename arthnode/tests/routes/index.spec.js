91326ec7fd0749cb240ff1133efd51bbc07ea902
Arthur Gorr <art.gorr@gmail.com>
2016-03-13 15:14:15 -0500

    Got a few route tests running, not very interesting TODO: have /about return some JSON, and test for that

--- arthnode/tests/routes/index.spec.js
+++ arthnode/tests/routes/index.spec.js
@@ -1,7 +1,40 @@
-describe("ArthNode server", function() {
-    describe("[GET] /about", function() {
-        it("returns status code 200", function() {
+var request = require("request");
 
+var base_url = "http://localhost:3000/"
+
+describe("ArthNode server", function() {
+    describe("[GET] /", function() {
+        it("returns status code 200", function(done) {
+            request.get(base_url, function(error, response, body) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
         });
+
+        it("returns Hello World", function(done) {
+            request.get(base_url, function(error, response, body) {
+                expect(body).toBe("Hello World");
+                done();
+            });
+        });
+        
+    });
+    
+    describe("[GET] /about", function() {
+        it("returns status code 200", function(done) {
+            request.get(base_url+"about", function(error, response, body) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+
+/*
+        it("returns Hello World", function(done) {
+            request.get(base_url, function(error, response, body) {
+                expect(body).toBe("Hello World");
+                done();
+            });
+        });
+  */      
     });
 });
